e95b4c9279fd11326ca870b4af07579627f9254c
Mario <ggperpi@gmail.com>
2023-07-18 11:43:57 +0200

    transicion votos - historico

--- src/main/java/mgg/code/vista/Home.java
+++ src/main/java/mgg/code/vista/Home.java
@@ -751,14 +751,20 @@ public class Home extends JFrame {
     private void btnDesplegarVideoActionPerformed(ActionEvent actionEvent) {
         if (tipoElecciones == 1 || tipoElecciones == 2) {
             if (!desplegado) {
-                if (!votosIn) {
+                if (votosIn) {
                     if (oficiales) {
-                        ipf.congresoHistoricosEntra();
+                        ipf.congresoVotosSale();
                     } else {
-                        ipf.congresoSondeoHistoricosEntra();
+                        ipf.congresoSondeoVotosSale();
                     }
-                    historicosIn = true;
+                    votosIn = false;
                 }
+                if (oficiales) {
+                    ipf.congresoHistoricosEntra();
+                } else {
+                    ipf.congresoSondeoHistoricosEntra();
+                }
+                historicosIn = true;
             } else {
                 int position = tablaDatos.getSelectedRow();
                 if (position != -1) {
@@ -777,14 +783,21 @@ public class Home extends JFrame {
     private void btnDesplegarDirectoActionPerformed(ActionEvent actionEvent) {
         if (tipoElecciones == 1 || tipoElecciones == 2) {
             if (!desplegado) {
-                if (!historicosIn) {
+                if (historicosIn) {
                     if (oficiales) {
-                        ipf.congresoVotosEntra();
+                        ipf.congresoHistoricosSale();
                     } else {
-                        ipf.congresoSondeoVotosEntra();
+                        ipf.congresoSondeoHistoricosSale();
                     }
-                    votosIn = true;
+                    historicosIn = false;
                 }
+                if (oficiales) {
+                    ipf.congresoVotosEntra();
+                } else {
+                    ipf.congresoSondeoVotosEntra();
+                }
+                votosIn = true;
+
             } else {
                 if (tipoElecciones == 1) {
                     ipf.congresoMillonesEntra();
